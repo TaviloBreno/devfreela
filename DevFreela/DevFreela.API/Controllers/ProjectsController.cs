1749ab8c1974b6d091bc4591dceb709bb5fb4540
Tavilo Breno - PC4 <breno_wk2@hotmail.com>
2023-11-07 20:51:25 -0300

    Criando a Controller de projetos e o arquivo gitignore

---
+++ DevFreela/DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
